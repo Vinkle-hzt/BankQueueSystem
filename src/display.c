776150f87895198f5b02fa43919ae0e97c797b9a
weiyi-cmd <2829109149@qq.com>
2021-07-21 16:51:03 +0800

    okk

--- src/display.c
+++ src/display.c
@@ -1,1 +1,45 @@
 #include "display.h"
+
+void show_main_menu()
+{
+    printf(" ------------------------------------------------\n");
+    printf("|                                                |\n");
+    printf("|            1.Picking up a number               |\n");
+    printf("|            2.Handling business                 |\n");
+    printf("|            3.Logging out of the system         |\n");
+    printf("|            4.Trading summary                   |\n");
+    printf("|            0.Exit                              |\n");
+    printf("|                                                |\n");
+    printf(" ------------------------------------------------\n");
+}
+
+void show_basicbusiness_menu()
+{
+    printf(" ------------------------------------------------\n");
+    printf("|                                                |\n");
+    printf("|            1.Check balance                     |\n");
+    printf("|            2.Deposit                           |\n");
+    printf("|            3.Withdrawal                        |\n");
+    printf("|            4.Transfer                          |\n");
+    printf("|            5.Check transaction records         |\n");
+    printf("|            0.Close the deal                    |\n");
+    printf("|                                                |\n");
+    printf(" ------------------------------------------------\n");
+}
+
+void show_exit_menu()
+{
+    printf("              ******           ******            \n");
+    printf("           ***********       ***********         \n");
+    printf("          **************   **************        \n");
+    printf("         *********************************       \n");
+    printf("         ***********------------**********       \n");
+    printf("          **********|  Goodbye | *********        \n");
+    printf("           *********------------********         \n");
+    printf("             *************************           \n");
+    printf("               *********************             \n");
+    printf("                 ******************              \n");
+    printf("                   **************                \n");
+    printf("                     **********                  \n");
+    printf("                       ******                    \n");
+}
--- src/main.c
+++ src/main.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
+#include "display.h"
 
 int main()
 {
     printf("Welcome to Bank Queue System :) !\n");
+    show_main_menu();
+    int i = -1;
+    scanf("%d",&i);
+    if(i == 0){
+        show_exit_menu();
+    }
     return 0;
 }
