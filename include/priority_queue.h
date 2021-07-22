dfa53410b9a93b416ca4f7051d11e07f90c311bd
Vinkle-hzt <vinkle-hzt@outlook.com>
2021-07-22 14:13:31 +0800

    添加优先队列

---
+++ include/priority_queue.h
@@ -0,0 +1,78 @@
+#pragma once
+#define PQ_DEFAULT_SIZE 10
+
+#include <stdlib.h>
+#include <string.h>
+#include "helpful_func.h"
+
+/**
+ * @brief 优先队列类，能够自动扩充容量
+ * 
+ */
+typedef struct
+{
+    int maxSize;                     ///< 最大容量
+    int curSize;                     ///< 当前容量
+    void **data;                     ///< 数据数组
+    int (*CompFunc)(void *, void *); ///< 比较函数
+} priority_queue;
+
+/**
+ * @brief 
+ * 
+ * @param CompFunc 
+ * @return priority_queue* 
+ */
+priority_queue *pq_create(int (*CompFunc)(const void *, const void *));
+
+/**
+ * @brief 
+ * 
+ * @param pq 
+ */
+void pq_resize(priority_queue *pq);
+
+/**
+ * @brief 
+ * 
+ * @param pq 
+ * @param i 
+ */
+void pq_heapify(priority_queue *pq, int i);
+
+/**
+ * @brief 
+ * 
+ * @param pq 
+ * @param elem 
+ */
+void pq_push(priority_queue *pq, void *elem);
+
+/**
+ * @brief 
+ * 
+ * @param pq 
+ */
+void pq_pop(priority_queue *pq);
+
+/**
+ * @brief 
+ * 
+ * @param pq 
+ * @return void* 
+ */
+void *pq_top(priority_queue *pq);
+
+/**
+ * @brief 
+ * 
+ * @param pq 
+ */
+void pq_free(priority_queue *pq);
+
+/**
+ * @brief 
+ * 
+ * @param pq 
+ */
+void pq_clear(priority_queue *pq);
---
+++ src/priority_queue.c
@@ -0,0 +1,86 @@
+#include "priority_queue.h"
+
+priority_queue *pq_create(int (*CompFunc)(const void *, const void *))
+{
+    priority_queue *pq = malloc(sizeof(priority_queue));
+    pq->curSize = 0;
+    pq->maxSize = PQ_DEFAULT_SIZE;
+    pq->data = malloc(sizeof(void *) * PQ_DEFAULT_SIZE);
+    pq->CompFunc = CompFunc;
+    return pq;
+}
+
+void pq_resize(priority_queue *pq)
+{
+    void **tmp = pq->data;
+    pq->data = malloc(sizeof(void*) * pq->maxSize * 2);
+    memcpy(pq->data, tmp, sizeof(void *) * (pq->maxSize));
+    pq->maxSize *= 2;
+    free(tmp);
+}
+
+void pq_heapify(priority_queue *pq, int i)
+{
+    int left = i * 2 + 1;
+    int right = i * 2 + 2;
+    int largest = i;
+    if (left < pq->curSize && pq->CompFunc(pq->data[left], pq->data[i]) > 0)
+        largest = left;
+    if (right < pq->curSize && pq->CompFunc(pq->data[right], pq->data[largest]) > 0)
+        largest = right;
+    if (largest != i)
+    {
+        swab(&pq->data[i], &pq->data[largest], sizeof(void *));
+        pq_heapify(pq, largest);
+    }
+}
+
+void pq_push(priority_queue *pq, void *elem)
+{
+    if (pq->curSize == pq->maxSize)
+    {
+        pq_resize(pq);
+    }
+
+    int i = pq->curSize;
+    int fa = (i - 1) / 2;
+
+    pq->data[i] = elem;
+
+    while (i && pq->CompFunc(pq->data[fa], pq->data[i]) < 0)
+    {
+        swab(&pq->data[i], &pq->data[fa], sizeof(void *));
+        i = fa;
+        fa = (i - 1) / 2;
+    }
+    pq->curSize++;
+}
+
+void pq_pop(priority_queue *pq)
+{
+    pq->data[0] = pq->data[pq->curSize - 1];
+    pq->curSize--;
+    pq_heapify(pq, 0);
+}
+
+void *pq_top(priority_queue *pq)
+{
+    return pq->data[0];
+}
+
+void pq_free(priority_queue *pq)
+{
+    pq_clear(pq);
+    safe_free(pq->data);
+    safe_free(pq);
+}
+
+void pq_clear(priority_queue *pq)
+{
+    while(pq->curSize)
+    {
+        void* cur = pq_top(pq);
+        pq_pop(pq);
+        safe_free(cur);
+    }
+}
